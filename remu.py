d87f0eed9e0e791eead5ebec3183b857389d087d
Klimov Nikolay <klimov.ns@mail.ru>
2017-09-17 02:49:29 +0300

    Add 1h button [#2]

--- remu.py
+++ remu.py
@@ -23,10 +23,22 @@ chat_id = 0
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    text = engine.handle_text_message(message.text)
-    bot.send_message(message.chat.id, text)
     global chat_id
+    if chat_id != message.chat.id:
+        logging.info('chat_id changed!')
     chat_id = message.chat.id
+    handle_user_message(message.text)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.message:
+        handle_user_message(call.data)
+
+
+def handle_user_message(message_text):
+    text = engine.handle_text_message(message_text)
+    bot.send_message(chat_id, text)
 
 
 # temporal solution
@@ -34,8 +46,15 @@ def run_check_loop():
     while True:
         text = engine.check_for_message()
         if text != "":
-            bot.send_message(chat_id, text)
+            send_message(text)
         time.sleep(0.5)
+
+def send_message(message_text):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    callback_data = '1h ' + message_text
+    callback_button = telebot.types.InlineKeyboardButton(text="1h", callback_data=callback_data)
+    keyboard.add(callback_button)
+    bot.send_message(chat_id, message_text, reply_markup=keyboard)
 
 if __name__ == '__main__':
     # engine.register_action_callback( lambda text: bot.send_message(chat_id, text))
