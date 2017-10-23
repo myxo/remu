13866af8073a35c3731a208af662422788d53b19
Klimov Nikolay <klimov.ns@mail.ru>
2017-10-23 16:25:23 +0300

    Remove day of the week row

--- telegramcalendar.py
+++ telegramcalendar.py
@@ -9,12 +9,6 @@ def create_calendar(year,month):
     row=[]
     row.append(types.InlineKeyboardButton(calendar.month_name[month]+" "+str(year),callback_data="ignore"))
     markup.row(*row)
-    #Second row - Week Days
-    week_days=["M","T","W","R","F","S","U"]
-    row=[]
-    for day in week_days:
-        row.append(types.InlineKeyboardButton(day,callback_data="ignore"))
-    markup.row(*row)
 
     my_calendar = calendar.monthcalendar(year, month)
     for week in my_calendar:
